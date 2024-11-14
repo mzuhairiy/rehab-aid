55befba01159171b5e050e41e2b715b906cdc0d7
Muhammad Zuhairi <zuhairimuhammad13@gmail.com>
2024-11-14 23:28:28 +0700

    fix: layouting

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,8 @@
 import './app.css';
+import Layout from './layout/Layout';
 
 function App() {
-  return (
-    <>
-      <h1>Hello react + vite app</h1>
-    </>
-  )
+  return <Layout />
 }
 
 export default App
