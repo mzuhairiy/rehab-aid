7cd1c2e0b8c6f43860737f10aafed31c876d0182
mzuhairiy <muhammad.zuhairi@erraipasifik.com>
2025-02-11 02:20:54 +0000

    feat: add doctor's page
    revamp doctor's page and its details

---
+++ frontend/src/pages/Doctors/DoctorsAbout.jsx
@@ -0,0 +1,81 @@
+import { formatDate } from "../../utils/formatDate";
+
+const DoctorsAbout = () => {
+  return (
+    <div>
+      <div>
+        <h3
+          className="text-[20px] leading-[30px] text-headingColor font-semibold flex items-center
+            gap-2"
+        >
+          About of
+          <span className="text-irisBlueColor font-bold text-[24px] leading-9">
+            Muhammad Zuhairi
+          </span>
+        </h3>
+        <p className="text__para">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis
+          veritatis ipsa esse fuga eaque quo maxime nam expedita beatae qui,
+          error cum cumque voluptates temporibus. Ipsam laborum dolore
+          laboriosam! Non.
+        </p>
+      </div>
+
+      <div className="mt-12">
+        <h3 className="text-[20px] leading-[30px] text-headingColor font-semibold">
+          Education
+        </h3>
+
+        <ul className="pt-4 md:p-5">
+          <li className="flex flex-col sm:flex-row sm:justify-between sm:items-end md:gap-5 mb-[30px]">
+            <div>
+              <span className="text-irisBlueColor text-[15px] leading-6 font-semibold">
+                {formatDate("12-04-2008")} - {formatDate("08-03-2012")}
+              </span>
+              <p className="text-[16px] leading-6 font-medium text-textColor">
+                PHD in Surgeon
+              </p>
+            </div>
+            <p className="text-[14px] leading-5 font-medium text-textColor">
+              New Appolo Hospital, New York.
+            </p>
+          </li>
+        </ul>
+      </div>
+
+      <div className="mt-12">
+        <h3 className="text-[20px] leading-[30px] text-headingColor font-semibold">
+          Experience
+        </h3>
+
+        <ul className="grid sm-grid-cols-2 gap-[30px] pt-4 md:p-5">
+          <li className="p-4 rounded bg-[#fff9ea]">
+            <span className="text-yellowColor text-[15px] leading-6 font-semibold">
+              {formatDate("12-04-2012")} - {formatDate("02-04-2015")}
+            </span>
+            <p className="text-[16px] leading-6 font-medium text-textColor">
+              Surgeon
+            </p>
+
+            <p className="text-[16px] leading-6 font-medium text-textColor">
+              New Apollo Hospital, New York.
+            </p>
+          </li>
+          <li className="p-4 rounded bg-[#fff9ea]">
+            <span className="text-yellowColor text-[15px] leading-6 font-semibold">
+              {formatDate("12-04-2012")} - {formatDate("02-04-2015")}
+            </span>
+            <p className="text-[16px] leading-6 font-medium text-textColor">
+              Surgeon
+            </p>
+            <p className="text-[16px] leading-6 font-medium text-textColor">
+              New Apollo Hospital, New York.
+            </p>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default DoctorsAbout;
--- frontend/src/routes/Routers.jsx
+++ frontend/src/routes/Routers.jsx
@@ -4,25 +4,25 @@ import Services from "../pages/Services";
 import Login from "../pages/Login";
 import Signup from "../pages/Signup";
 import Contact from "../pages/Contact";
-import Psychiatrists from "../pages/Psychiatrists/Psychiatrists";
-import PsychiatristsDetails from "../pages/Psychiatrists/PsychiatristsDetails";
+import Doctors from "../pages/Doctors/Doctors";
+import DoctorsDetails from "../pages/Doctors/DoctorsDetails";
 
 import { Routes, Route } from "react-router-dom";
 
 const Routers = () => {
-    return (
-        <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/home" element={<Home />} />
-            <Route path="/psychiatrist" element={<Psychiatrists />} />
-            <Route path="/psychiatrist/:id" element={<PsychiatristsDetails />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/register" element={<Signup />} />
-            <Route path="/contact" element={<Contact />} />
-            <Route path="/services" element={<Services />} />
-            <Route path="/signup" element={<Signup />} />
-        </Routes>
-    );
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/home" element={<Home />} />
+      <Route path="/doctors" element={<Doctors />} />
+      <Route path="/doctors/:id" element={<DoctorsDetails />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Signup />} />
+      <Route path="/contact" element={<Contact />} />
+      <Route path="/services" element={<Services />} />
+      <Route path="/signup" element={<Signup />} />
+    </Routes>
+  );
 };
 
 export default Routers;
