51c2372030972f587d63b2cd713973ed7030b86d
Muhammad Zuhairi <zuhairimuhammad13@gmail.com>
2025-02-22 07:27:00 +0700

    feat: add login function

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,5 +1,8 @@
-import React, { useState } from "react";
-import { Link } from "react-router-dom";
+import { useState, useContext } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
+import { authContext} from '../context/AuthContext.jsx'
+import { BASE_URL } from "../../config";
 
 const Login = () => {
   const [formData, setFormData] = useState({
@@ -7,9 +10,53 @@ const Login = () => {
     password: "",
   });
 
+  const [loading, setLoading] = useState(false)
+  const navigate = useNavigate();
+  const { dispatch } = useContext(authContext);
+
   const handleInputChange = (e) => {
     setFormData({ ...formData, [e.target.name]: e.target.value });
   };
+
+  const submitHandler = async (event) => {
+      event.preventDefault();
+      setLoading(true);
+  
+      try {
+        const res = await fetch(`${BASE_URL}/auth/login`,{
+          method: 'POST',
+          headers:{
+            'Content-Type':'application/json'
+          },
+          body: JSON.stringify(formData)
+        });
+  
+        const result = await res.json();
+  
+        if(!res.ok){
+          throw new Error(result.message)
+        }
+        
+
+        dispatch({
+          type:'LOGIN_SUCCESS',
+          payload:{
+            user:result.data,
+            token:result.token,
+            role:result.role,
+          }
+        })
+
+        console.log(result, "login data")
+
+        setLoading(false)
+        toast.success(result.message)
+        navigate('/home')
+      } catch (err) {
+        toast.error(err.message)
+        setLoading(false)
+      }
+    };
 
   return (
     <section className="px-5 lg:px-0">
@@ -18,7 +65,7 @@ const Login = () => {
           Hello! <span className="text-primaryColor">Welcome.</span>
         </h3>
 
-        <form className="py-4 md:py-0">
+        <form className="py-4 md:py-0" onSubmit={submitHandler}>
           <div className="mb-5">
             <input
               type="email"
