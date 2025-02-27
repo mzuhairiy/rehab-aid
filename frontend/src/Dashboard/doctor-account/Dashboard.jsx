42a294f983173fa02b6bfd8a3a76741e29988283
mzuhairiy <muhammad.zuhairi@erraipasifik.com>
2025-02-27 06:29:55 +0000

    feat: add overveiw and profile sections

--- frontend/src/Dashboard/doctor-account/Dashboard.jsx
+++ frontend/src/Dashboard/doctor-account/Dashboard.jsx
@@ -6,7 +6,7 @@ import { BASE_URL } from "../../config.js";
 import Tabs from "../../dashboard/doctor-account/Tabs.jsx";
 import starIcon from "../../assets/images/Star.png";
 import DoctorAbout from "../../pages/Doctors/DoctorsAbout.jsx";
-import Profile from "../../dashboard/doctor-account/Profile.jsx";
+import Profile from "../../Dashboard/doctor-account/Profile.jsx";
 
 const Dashboard = () => {
   const { data, loading, error } = useGetProfile(
@@ -63,10 +63,10 @@ const Dashboard = () => {
                           className="bg-[#CCF0F3] text-irisBlueColor py-1 px-4 lg:py-2 lg:px-6 rounded
                         text-[12px] leading-4 lg:text-[16px] lg:leading-6 font-semibold"
                         >
-                          Surgeon
+                          {data.specialization}
                         </span>
                         <h3 className="text-[22px] leading-9 font-bold text-headingColor mt-3">
-                          Fariduddin
+                          {data.name}
                         </h3>
 
                         <div className="flex items-center gap-[6px]">
@@ -75,17 +75,17 @@ const Dashboard = () => {
                           leading-5 lg:text-[16px] lg:leading-6 font-semibold"
                           >
                             <img src={starIcon} alt="" />
-                            4.5
+                            {data.averageRating}
                           </span>
                           <span
                             className="text-textColor text-[14px] leading-5 lg:text-[16px]
                             lg:leading-6 font-semibold"
                           >
-                            (233)
+                            ({data.totalRating})
                           </span>
                         </div>
                         <p className="text__para font-[15px] lg:max-w-[390px] leading-6">
-                          Doctor Bio
+                          {data?.bio}
                         </p>
                       </div>
                     </div>
@@ -99,7 +99,7 @@ const Dashboard = () => {
                 )}
 
                 {tab === "appointments" && <div>appointments</div>}
-                {tab === "settings" && <Profile />}
+                {tab === "settings" && <Profile doctorData={data} />}
               </div>
             </div>
           </div>
