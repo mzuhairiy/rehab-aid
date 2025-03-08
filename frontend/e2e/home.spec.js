ba035d6443b0aec08b594145bdd584fcb75940f5
Muhammad Zuhairi <zuhairimuhammad13@gmail.com>
2025-03-08 15:45:26 +0700

    feat: edit home.spec.js url

--- frontend/e2e/home.spec.js
+++ frontend/e2e/home.spec.js
@@ -1,20 +1,23 @@
 import { test, expect } from "@playwright/test";
 
 test.describe("Homepage", () => {
+  let baseURL;
+
   test.beforeEach(async ({ page }) => {
-    await page.goto(process.env.BASE_URL || "http://localhost:5173"); // ✅ Handle kalau BASE_URL kosong
+    baseURL = process.env.BASE_URL;
+    await page.goto(baseURL);
     await expect(page).toHaveTitle("Rehab-Aid");
   });
 
   test("Should direct to find a doctor page", async ({ page }) => {
     await page.click("header li:nth-child(2) a:nth-child(1)");
-    await expect(page).toHaveURL("(`${baseURL}/doctors");
+    await expect(page).toHaveURL(`${baseURL}/doctors`);
     await expect(page.locator("h2[class='heading']")).toBeVisible();
   });
 
   test("Should direct to services page", async ({ page }) => {
     await page.click("header li:nth-child(3) a:nth-child(1)");
-    await expect(page).toHaveURL("(`${baseURL}/services");
+    await expect(page).toHaveURL(`${baseURL}/services`);
     await expect(
       page.locator("//h2[normalize-space()='Cancer Care']"),
     ).toBeVisible();
@@ -28,7 +31,7 @@ test.describe("Homepage", () => {
 
   test("Should direct to contact page", async ({ page }) => {
     await page.click("header li:nth-child(4) a:nth-child(1)");
-    await expect(page).toHaveURL("(`${baseURL}/contact");
+    await expect(page).toHaveURL(`${baseURL}/contact`);
     await expect(page.locator(".heading.text-center")).toContainText(
       "Contact Us",
     );
@@ -36,7 +39,7 @@ test.describe("Homepage", () => {
 
   test("Should direct to login page", async ({ page }) => {
     await page.click("//button[normalize-space()='Login']");
-    await expect(page).toHaveURL("(`${baseURL}/login");
+    await expect(page).toHaveURL(`${baseURL}/login`);
     await expect(
       page.locator(
         "h3[class='text-headingColor text-[22px] leading-9 font-bold mb-10']",
